3cd31f7eb4084032997d4d95de2366d5a8bfd865
Joel Mattieu <joelmattieu2016@gmail.com>
2023-01-14 14:11:19 -0300

    adding car cards

---
+++ js/componentes/get-all.js
@@ -0,0 +1,32 @@
+const $template = document.getElementById("template-cars").content,
+  $section = document.getElementById("box-cars"),
+  $fragment = document.createDocumentFragment();
+
+
+export default async function getAll() {
+  try {
+    const res = await fetch("../json/cars.json");
+    const json = await res.json();
+
+    if (!res.ok) throw { status: res.status, statusText: res.statusText};
+    
+    json.cars.forEach(el => {
+      $template.querySelector(".car-name").textContent = el.brand;
+      $template.querySelector(".year").textContent = `Year: ${el.year}`;
+      $template.querySelector(".km").textContent = `KM: ${el.km}`;
+      $template.querySelector(".prize").textContent = `$${el.prize}`;
+
+      let $clone = document.importNode($template, true);
+      $fragment.appendChild($clone);
+    });
+
+    $section.appendChild($fragment);
+
+
+
+  } catch (error) {
+    let message = error.statusText || "Error ocurred";
+    $section.insertAdjacentHTML("afterend", `<p><b>Error: ${error.status}: ${message}</b></p>`);
+    console.error(`Error: ${error.status}: ${message}`);
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 import hamburgerMenu from "./componentes/hamburger.js";
+import getAll from "./componentes/get-all.js";
 
 
 document.addEventListener("DOMContentLoaded", e => {
   hamburgerMenu();
+  getAll();
 });
+
+
